7cfc26bdc1f4908ec3b259b6f6c02bedd33c69db
BiancaCornaglia <bian.cornaglia11@gmail.com>
2024-07-02 01:24:25 -0300

    no se ven los pj

--- scenes/Menu.js
+++ scenes/Menu.js
@@ -8,10 +8,10 @@ export default class Menu extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image("inicio", " ./public/assets/Logo.png")
+        //this.load.image("inicio", " ./public/assets/Logo.png")
         this.load.image("sky", "./public/assets/sky.jpg");
         this.load.image("platform", "./public/assets/suelo.png");
-        this.load.image("girl", "./public/assets/girl.png");
+        this.load.image("girl", "../public/assets/girl.png");
         //this.load.image("rainbow", "./public/assets/piñata01.png");
         //this.load.image("animal", "./public/assets/piñata02.png");
         //this.load.image("star", "./public/assets/piñata03.png");
@@ -26,8 +26,8 @@ export default class Menu extends Phaser.Scene {
         this.load.image("sugar", "./public/assets/AZUCAR.png");
         this.load.image("menu", "./public/assets/menu.jpg");
         this.load.spritesheet("sugarbar", "./public/assets/sugarbar.png", { frameWidth: 300, frameHeight: 100 });
-        this.load.spritesheet("girlmove", "./public/assets/Girlwalk.png", { frameWidth: 90, frameHeight: 90 });
-        this.load.spritesheet("girljump", "./public/assets/Girljump.png", { frameWidth: 105, frameHeight: 90 });
+        this.load.spritesheet("girlmove", "../public/assets/Girlwalk.png", { frameWidth: 90, frameHeight: 90 });
+        this.load.spritesheet("girljump", "../public/assets/Girljump.png", { frameWidth: 105, frameHeight: 90 });
     }
 
     create() {
